16a0d93caf8cd9ff081b2988338bdd5b8cbf8544
Lxg1c <teran_2005@mail.ru>
2025-02-22 12:40:11 +0300

    added router

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
+from views.user import router as user_router
 
 app = FastAPI()
+app.include_router(user_router)
+
 
 @app.get("/")
 def hello_index():
     return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
