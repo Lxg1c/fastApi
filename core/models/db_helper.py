f21efbf7f4ff857cf5368328eda2ddbc61a0df1c
Lxg1c <teran_2005@mail.ru>
2025-03-01 14:19:30 +0300

    added scoped session dependency

--- core/models/db_helper.py
+++ core/models/db_helper.py
@@ -1,23 +1,19 @@
 from asyncio import current_task
-
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
     async_scoped_session,
     AsyncSession,
 )
-
 from core.config import settings
 
 
 class DbHelper:
-
     def __init__(self, url, echo=False):
         self.engine = create_async_engine(
             url=url,
             echo=echo,
         )
-
         self.session_factory = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
@@ -26,17 +22,22 @@ class DbHelper:
         )
 
     def get_scoped_session(self):
-        session = async_scoped_session(
+        return async_scoped_session(
             session_factory=self.session_factory,
             scopefunc=current_task,
         )
 
-        return session
-
     async def session_dependency(self) -> AsyncSession:
-        async with self.get_scoped_session() as session:
+        async with self.session_factory() as session:
             yield session
-            await session.remove()
+            await session.aclose()  # Используем aclose() для асинхронного закрытия
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.get_scoped_session()
+        try:
+            yield session
+        finally:
+            await session.aclose()  # Закрываем сессию после завершения
 
 
 db_helper = DbHelper(
