545285f72a99ec1cfb18bb579f377c3020b29fc5
Lxg1c <teran_2005@mail.ru>
2025-03-01 13:51:30 +0300

    initialized db_helper

---
+++ core/models/db_helper.py
@@ -0,0 +1,45 @@
+from asyncio import current_task
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
+
+from core.config import settings
+
+
+class DbHelper:
+
+    def __init__(self, url, echo=False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            autoflush=False,
+            autocommit=False,
+            expire_on_commit=False,
+        )
+
+    def get_scoped_session(self):
+        session = async_scoped_session(
+            session_factory=self.session_factory,
+            scopefunc=current_task,
+        )
+
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.get_scoped_session() as session:
+            yield session
+            await session.remove()
+
+
+db_helper = DbHelper(
+    url=settings.db_url,
+    echo=settings.db_echo,
+)
