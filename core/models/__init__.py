87c0da8c7e3d0f208c4cd10c241857e354341785
Lxg1c <teran_2005@mail.ru>
2025-03-09 22:22:05 +0300

    Added User

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     "Base",
     "Product",
+    "User",
     "DbHelper",
 )
 
 from .base import Base
 from .db_helper import DbHelper
 from .product import Product
+from .user import User
