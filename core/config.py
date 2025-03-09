5f5aed8f4823b493c15a5a56ba6c0632a1ae9bf3
Lxg1c <teran_2005@mail.ru>
2025-03-09 19:51:05 +0300

    Added auth jwt settings

--- core/config.py
+++ core/config.py
@@ -4,6 +4,19 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent.parent
 
 
+class DbSettings(BaseSettings):
+    url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    echo: bool = True
+
+
+class AuthJWT(BaseSettings):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "HS256"
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 30
+
+
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
