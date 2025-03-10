dcddbc4ed158721a75d0414f5ba332bec0ce2716
Lxg1c <teran_2005@mail.ru>
2025-03-11 00:53:27 +0300

    initialized schemas

---
+++ api_v1/users/schemas.py
@@ -0,0 +1,19 @@
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    password: bytes
+    username: Annotated[str, MinLen(3), MaxLen(20)]
+
+
+class CreateUser(UserBase):
+    pass
+
+
+class UserSchema(UserBase):
+    id: int
+
+    model_config = ConfigDict(strict=True)
