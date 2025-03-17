dcba15f1d14723e0f3ba9bde11de6add7051a248
Lxg1c <teran_2005@mail.ru>
2025-03-17 22:54:40 +0300

    initialized image model

--- core/models/image.py
+++ core/models/image.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import Mapped
 from .base import Base
 
 
-class ImageModel(Base):
+class Image(Base):
+    __tablename__ = "images"
+
     product_id: Mapped[int]
     url: Mapped[str]
+    is_primary: Mapped[bool]
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id})"
+
+    def __repr__(self):
+        return str(self)
