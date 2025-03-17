4111de6adaf47d6d2579be4de2570b1d21d5c292
Lxg1c <teran_2005@mail.ru>
2025-03-17 19:49:00 +0300

    Refactored views

--- api_v1/products/views.py
+++ api_v1/products/views.py
@@ -1,41 +1,45 @@
 # views.py
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from .schemas import Product, ProductCreate, ProductUpdate, ProductUpdatePartial
+from .schemas import ProductCreate, ProductUpdate, ProductUpdatePartial, ProductSchema
 from core.models.db_helper import db_helper
 from . import crud
 from .dependencies import get_product_by_id
+from fastapi import status
 
-router = APIRouter(tags=["Products"])
+router = APIRouter(
+    prefix="/products",
+    tags=["Products"],
+)
 
 
-@router.get("/", response_model=list[Product])
+@router.get("/", response_model=list[ProductSchema])
 async def get_products(
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.get_products(session=session)
 
 
-@router.post("/", response_model=Product)
+@router.post("/", response_model=ProductSchema)
 async def create_product(
     product_in: ProductCreate,
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.create_product(session=session, product_in=product_in)
 
 
-@router.get("/{product_id}", response_model=Product)
+@router.get("/{product_id}", response_model=ProductSchema)
 async def get_product(
-    product: Product = Depends(get_product_by_id),
+    product: ProductSchema = Depends(get_product_by_id),
 ):
     return product
 
 
-@router.put("/{product_id}", response_model=Product)
+@router.put("/{product_id}", response_model=ProductSchema)
 async def update_product(
     product_update: ProductUpdate,
-    product_in: Product = Depends(get_product),
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    product_in: ProductSchema = Depends(get_product),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.update_product(
         product_in=product_in,
@@ -45,26 +49,23 @@ async def update_product(
     )
 
 
-@router.patch("/{product_id}", response_model=Product)
+@router.patch("/{product_id}", response_model=ProductSchema)
 async def update_product_partial(
-    product_update: ProductUpdatePartial,  # Для частичных обновлений используем ProductUpdatePartial
-    product_in: Product = Depends(get_product),
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    product_update: ProductUpdatePartial,
+    product_in: ProductSchema = Depends(get_product),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.update_product(
         product_in=product_in,
         session=session,
         product_update=product_update,
-        partial=True,  # Частичное обновление
+        partial=True,
     )
-
-
-from fastapi import HTTPException, status
 
 
 @router.delete("/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_product(
-    product: Product = Depends(get_product_by_id),
-    session: AsyncSession = Depends(db_helper.session_dependency),
+    product: ProductSchema = Depends(get_product_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ) -> None:
     await crud.delete_product(session=session, product=product)
