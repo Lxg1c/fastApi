ce478704ff1b414f754b98d33ab7c85fe2673b3c
Lxg1c <teran_2005@mail.ru>
2025-03-17 22:45:26 +0300

    initialized

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from .products.views import router as product_router
-from .users.views import router as auth_router
+from .category.views import router as category_router
+from .users.views import router as users_router
 
 router = APIRouter()
 router.include_router(product_router)
-router.include_router(auth_router)
+router.include_router(users_router)
+router.include_router(category_router)
