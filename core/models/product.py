d4a555fe0853e212efc0efc0a57aa3d06b5b9ba3
Lxg1c <teran_2005@mail.ru>
2025-03-18 12:54:31 +0300

    Added relation to image

--- core/models/product.py
+++ core/models/product.py
@@ -1,12 +1,18 @@
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column
 from .base import Base
 
 
 class Product(Base):
     __tablename__ = "products"
 
-    name: Mapped[str]
-    price: Mapped[int]
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    images: Mapped[list["Image"]] = relationship(
+        "Image", back_populates="product", cascade="all, delete-orphan"
+    )
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, name={self.name!r})"
