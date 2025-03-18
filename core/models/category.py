27e5c2fab750680ca80477813ee1b646e2fd5454
Lxg1c <teran_2005@mail.ru>
2025-03-18 18:21:24 +0300

    Added relationship between product and category.py

--- core/models/category.py
+++ core/models/category.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
@@ -7,6 +7,10 @@ class Category(Base):
     __tablename__ = "categorys"
 
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    products: Mapped[list["Product"]] = relationship(
+        "Product",
+        back_populates="category",
+    )
 
     def __str__(self):
         return f"{self.__class__.__name__}(id={self.id}, name={self.name!r})"
