a0b7606f7a8d68a3a685845635ade5c1d2543253
Lxg1c <teran_2005@mail.ru>
2025-03-21 19:01:36 +0300

    Added middleware

--- main.py
+++ main.py
@@ -5,18 +5,34 @@ from core.config import settings
 from core.models import Base
 from core.models.db_helper import db_helper
 from api_v1 import router as api_v1
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(_: FastAPI):
     async with db_helper.engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_v1, prefix=settings.api_v1_prefix)
 
-
+# Запуск сервера
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
