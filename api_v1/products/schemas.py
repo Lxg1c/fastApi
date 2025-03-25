1bf7aed80a36fd9f30829f3b3eed07d936cf59f0
Lxg1c <teran_2005@mail.ru>
2025-03-25 17:22:10 +0300

    added columns to schemas

--- api_v1/products/schemas.py
+++ api_v1/products/schemas.py
@@ -1,8 +1,10 @@
 from typing import Optional, List
-
 from pydantic import BaseModel, ConfigDict
-
 from api_v1.images.schemas import ImageBase
+
+
+class ProductSizeSchema(BaseModel):
+    size: str
 
 
 class ProductBase(BaseModel):
@@ -14,12 +16,16 @@ class ProductSchema(ProductBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
     category_id: int
+    description: str
     images: Optional[List[ImageBase]] = []
+    sizes: List[ProductSizeSchema] = []
 
 
 class ProductCreate(ProductBase):
+    description: str
     images: Optional[List[ImageBase]] = []
     category_id: int
+    sizes: List[str]
 
 
 class ProductUpdate(ProductCreate):
@@ -28,4 +34,6 @@ class ProductUpdate(ProductCreate):
 
 class ProductUpdatePartial(ProductBase):
     name: str | None = None
+    description: str | None = None
     price: int | None = None
+    sizes: Optional[List[str]] = None
