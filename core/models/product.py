33e3bd2e6732f00ed4633ba647acc99bcff37365
Lxg1c <teran_2005@mail.ru>
2025-03-25 17:21:30 +0300

    created ProductSize table and added sizes, description column

--- core/models/product.py
+++ core/models/product.py
@@ -4,12 +4,25 @@ from sqlalchemy.orm import mapped_column
 from .base import Base
 
 
+class ProductSize(Base):
+    __tablename__ = "product_sizes"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
+    size: Mapped[str] = mapped_column(String, nullable=False)
+
+    product: Mapped["Product"] = relationship("Product", back_populates="sizes")
+
+
 class Product(Base):
     __tablename__ = "products"
 
     name: Mapped[str] = mapped_column(String, nullable=False)
     price: Mapped[int] = mapped_column(Integer, nullable=False)
-
+    description: Mapped[str] = mapped_column(String, nullable=True)
+    sizes: Mapped[list["ProductSize"]] = relationship(
+        "ProductSize", back_populates="product", cascade="all, delete-orphan"
+    )
     images: Mapped[list["Image"]] = relationship(
         "Image", back_populates="product", cascade="all, delete-orphan"
     )
