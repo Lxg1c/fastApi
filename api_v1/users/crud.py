74f1224f4372d57410788faf646ff6d449931e8b
Lxg1c <teran_2005@mail.ru>
2025-03-27 22:37:48 +0300

    added getter of user by email

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -1,28 +1,32 @@
 from typing import List
+
+from pydantic import EmailStr
 from sqlalchemy import select, Result
 from sqlalchemy.ext.asyncio import AsyncSession
-from api_v1.users.schemas import CreateUser, UserSchema
+
+from api_v1.users.schemas import CreateUser
 from core.models.user import User
+from core.services.shared.dependencies import get_all_records, create_record
 
 
 async def get_users(session: AsyncSession) -> List[User]:
-    stmt = select(User).order_by(User.id)
-    result: Result = await session.execute(stmt)
-    users = result.scalars().all()
-    return list(users)
+    return await get_all_records(session, User)
 
-async def get_user_by_username(session: AsyncSession, username: str,) -> User:
+
+async def get_user_by_username(session: AsyncSession, username: str, ) -> User:
     stmt = select(User).where(User.username == username)
     result: Result = await session.execute(stmt)
     return result.scalar_one_or_none()
 
-async def create_user(
-    session: AsyncSession,
-    user: CreateUser,
-) -> User:
-    new_user = User(**user.model_dump())
-    session.add(new_user)
-    await session.commit()
-    await session.refresh(new_user)
 
-    return new_user
+async def get_user_by_email(session: AsyncSession, email: EmailStr) -> User:
+    stmt = select(User).where(User.email == email)
+    result: Result = await session.execute(stmt)
+    return result.scalar_one_or_none()
+
+
+async def create_user(
+        session: AsyncSession,
+        user: CreateUser,
+) -> User:
+    return await create_record(session, User, user.model_dump())
