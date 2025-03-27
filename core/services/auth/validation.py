6032c4a377ef4fbf6c8763002be5e46026d99f21
Lxg1c <teran_2005@mail.ru>
2025-03-27 22:36:59 +0300

    added validation for registration

--- core/services/auth/validation.py
+++ core/services/auth/validation.py
@@ -1,13 +1,15 @@
 from fastapi import Depends, HTTPException, Form
 from fastapi.security import OAuth2PasswordBearer
 from jwt import InvalidTokenError
+from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
-from api_v1.users.crud import get_user_by_username
-from api_v1.users.schemas import UserSchema
+from api_v1.users.crud import get_user_by_username, get_user_by_email
+from api_v1.users.schemas import UserSchema, CreateUser
 from . import utils as auth_utils
 from .helpers import TOKEN_TYPE_FIELD, ACCESS_TOKEN_TYPE, REFRESH_TOKEN_TYPE
+from .utils import hash_password
 from ...models.db_helper import db_helper
 
 oauth2_scheme = OAuth2PasswordBearer(
@@ -16,7 +18,7 @@ oauth2_scheme = OAuth2PasswordBearer(
 
 
 def get_current_token_payload(
-    token: str = Depends(oauth2_scheme),
+        token: str = Depends(oauth2_scheme),
 ) -> dict:
     try:
         payload = auth_utils.decode_jwt(
@@ -31,8 +33,8 @@ def get_current_token_payload(
 
 
 def validate_token_type(
-    payload: dict,
-    token_type: str,
+        payload: dict,
+        token_type: str,
 ) -> bool:
     current_token_type = payload.get(TOKEN_TYPE_FIELD)
     if current_token_type == token_type:
@@ -44,7 +46,7 @@ def validate_token_type(
 
 
 async def get_user_by_token_sub(
-    payload: dict, session: AsyncSession = Depends(db_helper.scoped_session_dependency)
+        payload: dict, session: AsyncSession = Depends(db_helper.scoped_session_dependency)
 ) -> UserSchema:
     username: str | None = payload.get("sub")
 
@@ -67,7 +69,7 @@ async def get_user_by_token_sub(
 
 def get_auth_user_from_token_of_type(token_type: str):
     async def get_auth_user_from_token(
-        payload: dict = Depends(get_current_token_payload),
+            payload: dict = Depends(get_current_token_payload),
     ) -> UserSchema:
         validate_token_type(payload, token_type)
         return await get_user_by_token_sub(payload)
@@ -80,8 +82,8 @@ class UserGetterFromToken:
         self.token_type = token_type
 
     def __call__(
-        self,
-        payload: dict = Depends(get_current_token_payload),
+            self,
+            payload: dict = Depends(get_current_token_payload),
     ):
         validate_token_type(payload, self.token_type)
         return get_user_by_token_sub(payload)
@@ -91,21 +93,36 @@ get_current_auth_user = get_auth_user_from_token_of_type(ACCESS_TOKEN_TYPE)
 get_current_auth_user_for_refresh = UserGetterFromToken(REFRESH_TOKEN_TYPE)
 
 
-def get_current_active_auth_user(
-    user: UserSchema = Depends(get_current_auth_user),
-):
-    if user.active:
-        return user
-    raise HTTPException(
-        status_code=status.HTTP_403_FORBIDDEN,
-        detail="inactive user",
-    )
+async def validate_registration(
+        username: str = Form(),
+        email: EmailStr = Form(),
+        password: str = Form(),
+        session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> CreateUser:
+    user = await get_user_by_username(username=username, session=session)
+
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Username is already registered",
+        )
+
+    user = await get_user_by_email(email=email, session=session)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Email is already registered",
+        )
+
+    password = hash_password(password=password)
+
+    return CreateUser(username=username, email=email, password=password)
 
 
 async def validate_auth_user(
-    username: str = Form(),
-    password: str = Form(),
-    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+        username: str = Form(),
+        password: str = Form(),
+        session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     unauthorized_exc = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -117,8 +134,8 @@ async def validate_auth_user(
         raise unauthorized_exc
 
     if not auth_utils.validate_password(
-        password=password,
-        hashed_password=user.password,
+            password=password,
+            hashed_password=user.password,
     ):
         raise unauthorized_exc
 
