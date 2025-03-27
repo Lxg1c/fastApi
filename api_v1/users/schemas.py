96b4b8c9abbdae5d32c7d41e7ee07248a2bb3241
Lxg1c <teran_2005@mail.ru>
2025-03-27 22:37:26 +0300

    moved password to Create User Schema

--- api_v1/users/schemas.py
+++ api_v1/users/schemas.py
@@ -1,16 +1,16 @@
 from typing import Annotated
+
 from annotated_types import MinLen, MaxLen
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class UserBase(BaseModel):
     email: EmailStr
-    password: bytes
     username: Annotated[str, MinLen(3), MaxLen(20)]
-    active: bool = True
 
 
 class CreateUser(UserBase):
+    password: bytes
     pass
 
 
