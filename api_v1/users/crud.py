45e16ba353eeb91bde7c1f0d739592162126ba20
Lxg1c <teran_2005@mail.ru>
2025-04-07 23:09:24 +0300

    refactor: add comms

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -6,19 +6,27 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import CreateUser
 from core.models.user import User
-from core.services.shared.dependencies import get_all_records, create_record
+from core.services.shared.dependencies import get_all_records, create_record, delete_record
 
 
+# Получить всех пользователей
 async def get_users(session: AsyncSession) -> List[User]:
     return await get_all_records(session, User)
 
 
-async def get_user_by_username(session: AsyncSession, username: str, ) -> User:
+# Получить пользователя по id
+async def get_user_by_id(session: AsyncSession, user_id: int) -> User | None:
+    return await session.get(User, user_id)
+
+
+# Получить пользователя по username
+async def get_user_by_username(session: AsyncSession, username: str) -> User:
     stmt = select(User).where(User.username == username)
     result: Result = await session.execute(stmt)
     return result.scalar_one_or_none()
 
 
+# Получить пользователя по email
 async def get_user_by_email(session: AsyncSession, email: EmailStr) -> User:
     stmt = select(User).where(User.email == email)
     result: Result = await session.execute(stmt)
