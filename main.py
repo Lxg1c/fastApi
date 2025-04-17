14025a8bdc5c38fe3053162620ec2a9e8c333a52
Lxg1c <teran_2005@mail.ru>
2025-04-17 17:34:16 +0300

    fix: add origins

--- main.py
+++ main.py
@@ -24,9 +24,15 @@ app = FastAPI(lifespan=lifespan)
 
 # Разрешённые источники для CORS (например, фронтенд на React)
 origins = [
+    "http://localhost:5176",
+    "http://127.0.0.1:5176",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+    "http://localhost:5175",
+    "http://127.0.0.1:5175",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:3000"
+    "http://localhost:3000",
 ]
 
 # Добавляем middleware CORS
