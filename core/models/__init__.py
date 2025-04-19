6e00c5b47ea18827641ddbebf2c0624fc9096d90
Lxg1c <teran_2005@mail.ru>
2025-04-19 16:56:09 +0300

    feat: add favorite

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -6,12 +6,14 @@ __all__ = (
     "DatabaseHelper",
     "Image",
     "Cart",
+    "Favorite",
 )
 
 from .base import Base
 from .cart import Cart
 from .category import Category
 from .db_helper import DatabaseHelper
+from .favorite import Favorite
 from .image import Image
 from .product import Product
 from .user import User
